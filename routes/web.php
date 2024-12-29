6895a5d2b8b5f82617a59507d683d3f79c527b38
unknown <fitrayanaf15@gmail.com>
2024-12-29 13:58:48 +0700

    new commit

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware([Authenticate::class])->group(function () {
     Route::get('/', function () {
-        return redirect('/login'); // Langsung arahkan ke Filament
+        return redirect('/admin/login'); // Langsung arahkan ke Filament
     });
 });
